eb7c9ea54aea40eb12d67b3b529f1580960dc002
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-05-09 14:26:33 -0300

    scrip movimiento player
    Se programo el movimiento con el stick digital del jugador hacia las 4 direcciones (no diagonal) del player (Hansel)

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -2,37 +2,93 @@ extends KinematicBody2D
 
 var SPEED = 300
 var velocity = Vector2()
+var new_motion_vector = Vector2(0,0)
+var pos_stick_angle
 
-
+func _ready():
+	# Obtengo el nodo llamado StickDigital buscando en el padre (la escena WorldTest en este caso)
+	# Si lo encuentra, lo conecto con el mismo (self) para recibir señales
+	# en caso de que no lo encuentre, no hará la conección y no habrá movimiento
+	var stickDigital = get_parent().get_node_or_null("StickDigital")
+	if stickDigital != null:
+		stickDigital.connect("stick_motion", self, "get_motion_vector")
+		
 func adrenalin():
 	SPEED = SPEED * 1.5
 	pass
 
+# Funcion que se ejecuta al recibir la señal "get_motion"
+# este obtiene el vector "dirección" (hacia donde apunta)
+func get_motion_vector(motion):
+	new_motion_vector = motion
+	
+
+	
+	
+	
+
 func _physics_process(delta):
-	if Input.is_action_just_released("ui_down") or Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
-		velocity.x = 0
-		velocity.y = 0
-		$AnimatedSprite.play("Idle")
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-		velocity.y = 0
-		$AnimatedSprite.play("RunRight")
-		$AnimatedSprite.flip_h = true
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		velocity.y = 0
+	# Usando el vector dirección recibido, multiplica para aumentar la fuerza de movimiento
+	pos_stick_angle = rad2deg(  new_motion_vector.angle_to(Vector2(1,0))  )
+	
+	#Movimiento derecha
+	if pos_stick_angle < 45 and pos_stick_angle >-45:
+		velocity = Vector2(SPEED,0)
 		$AnimatedSprite.play("RunRight")
 		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -SPEED
-		velocity.x = 0
-		$AnimatedSprite.play("RunTop")
-	if Input.is_action_pressed("ui_down"):
-		velocity.y = SPEED
-		velocity.x = 0
+	#Movimiento izquierda
+	if pos_stick_angle < -128 and pos_stick_angle > -179 or pos_stick_angle < 179 and pos_stick_angle > 134:
+		velocity = Vector2(-SPEED, 0)
+		$AnimatedSprite.play("RunRight")
+		$AnimatedSprite.flip_h = true
+		
+	#Movimiento abajo	
+	if pos_stick_angle > -128 and pos_stick_angle < -45:
+		velocity = Vector2(0, SPEED)
 		$AnimatedSprite.play("RunBot")
+		
+	
+	#Movimiento arriba
+	if pos_stick_angle > 45 and pos_stick_angle < 133:
+		velocity = Vector2(0, -SPEED)
+		$AnimatedSprite.play("RunTop")
+		
+	#Cuando se suelte el stick, se detiene el movimiento
+	if pos_stick_angle == 0:
+		velocity = Vector2(0, 0)
+		$AnimatedSprite.play("Idle")
+	
+	
+	print (pos_stick_angle)
+		
 	move_and_slide(velocity)
 	pass
+
+	
+	
+	
+	
+	#if Input.is_action_just_released("ui_down") or Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
+		#velocity.x = 0
+		#velocity.y = 0
+		
+	#if Input.is_action_pressed("ui_left"):
+		#velocity.x = -SPEED
+		#velocity.y = 0
+		
+	#if Input.is_action_pressed("ui_right"):
+		#velocity.x = SPEED
+		#velocity.y = 0
+		
+	#if Input.is_action_pressed("ui_up"):
+		#velocity.y = -SPEED
+		#velocity.x = 0
+		
+	#if Input.is_action_pressed("ui_down"):
+		#velocity.y = SPEED
+		#velocity.x = 0
+		
+	
 
 
 func _on_Area2D_area_entered(area):
