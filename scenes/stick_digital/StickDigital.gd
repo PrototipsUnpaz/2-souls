b50006bae31e5f91f265fd931b86cc334d447d61
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-05-09 12:09:31 -0300

    Scrip modificado
    Se agrego el codigo para el funcionamiento del stickdigital

--- scenes/stick_digital/StickDigital.gd
+++ scenes/stick_digital/StickDigital.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
+var touched = false
+const max_button_ration_length = 40
+signal stick_motion
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _process(_delta):
+	if touched == true:
+		if Input.is_action_just_released("touch"):
+			touched = false
+		else:
+			# clamp devuelve el mismo vector pero reducido para que su
+			# distancia sea de max_button_ration_length o menos
+			var new_position = get_local_mouse_position().clamped(max_button_ration_length)
+			$ButtonImage.position = new_position
+			# Emite una señal enviando la nueva posición ( clampeada ) para ser utilizada luego por el personaje
+			emit_signal("stick_motion", new_position)
+	elif $ButtonImage.position != $CenterPosition.position:
+		# Si no esta presionado y el botón no está centrado, lo centra
+		$ButtonImage.position = $CenterPosition.position
+		# Emite la misma señal que antes pero con la posición 0,0
+		emit_signal("stick_motion", $CenterPosition.position)
+	
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_TouchArea_input_event( _viewport, event, _shape_idx ):
+	if event.is_action_pressed("touch") && touched == false:
+		touched = true
