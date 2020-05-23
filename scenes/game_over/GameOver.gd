876908505c7f5acf343d09ee8a0d4e235d2f7d44
negrodraco65 <52358927+negrodraco65@users.noreply.github.com>
2020-05-23 04:44:03 -0300

    transicion escena Gameover
    Se creo la señal y la conexión de Gameover al morir Player.

---
+++ scenes/game_over/GameOver.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+
+
+
+func _ready():
+	pass 
+
+
+
+
+func _on_Reintentar_button_down():
+	get_tree().change_scene("res://scenes/WorldTest.tscn")
+	pass # Replace with function body.
