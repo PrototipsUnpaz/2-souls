5a4e42547738c63e60345c2d3b3bfced1bfdb30a
negrodraco65 <52358927+negrodraco65@users.noreply.github.com>
2020-05-25 18:31:12 -0300

    PowerMirror modificado
    El poder tiene movimiento.
    se elimina luego de un tiempo o al colisionar con enemy.

---
+++ scenes/power_gretel/PowerRed.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var direction = Vector2()
+export var vel = 200
+func _ready():
+	$Timer.start()
+	pass
+
+
+
+
+func _physics_process(delta):
+	if direction == Vector2(0,0):
+		direction = Vector2(-1,0)
+		move_local_x(direction.x * vel *delta)
+	move_local_x(direction.x * vel * delta)
+	if direction.y == 1:
+		move_local_y(direction.y * vel * delta);
+
+	else:
+		move_local_y(direction.y * vel * delta)
+
+	pass
+
+func _on_Timer_timeout():
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_PowerRed_area_entered(area):
+	queue_free()
+	pass # Replace with function body.
