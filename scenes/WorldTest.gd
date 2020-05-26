85b2d0dfb595a64f01b74fe6f0bc9eb21349629e
negrodraco65 <52358927+negrodraco65@users.noreply.github.com>
2020-05-26 20:26:59 -0300

    Ajustes de UI
    Se modificaron tamaños de colisiones y ubicaciones de boton y stick.

--- scenes/WorldTest.gd
+++ scenes/WorldTest.gd
@@ -7,9 +7,9 @@ func _ready():
 	pass
 	
 func _process(delta):
-	print(Autoload.count)
 	if Autoload.count == 2:
 		get_tree().change_scene("res://scenes/game_over/GameOver.tscn")
+		Autoload.count = 0
 	if Autoload.dead == true:
 		get_tree().change_scene("res://scenes/game_over/GameOver.tscn")
 		Autoload.dead = false
