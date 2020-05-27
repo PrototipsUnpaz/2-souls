9d5db51b421361cddf84e35bb2f5d2f25f7292f8
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-05-27 17:50:19 -0300

    mov provisorio enemy
    El movimiento del enemy funciona pero se bugea al stunearlo. Commit para exportar apk y luego proseguir.

--- scenes/enemy/Enemy.gd
+++ scenes/enemy/Enemy.gd
@@ -5,19 +5,38 @@ var posEnemy = 0
 var spd = 200
 var move = Vector2(spd, 0)
 
-func pathfinding():
+func pos_assign():
 
 	if self.position.x <=  72 and self.position.y >= 431:
-		move = Vector2(spd, 0)
-	if self.position.x >= 570 and self.position.y == 431:
-		move = Vector2(0, -spd)
+		
+		posEnemy = 1
+	if self.position.x >= 570 and self.position.y <= 431:
+		
+		posEnemy = 2
 	if self.position.y <= 315 and self.position.x >= 570:
-		move = Vector2(-spd, 0)
+		
+		posEnemy = 3
 	if self.position.y <= 315 and self.position.x <= 72:
+		
+		posEnemy = 4	
+	pass
+
+func pathfinding():
+	if posEnemy == 0:
+		move = Vector2(0,0)
+	if posEnemy == 1:
+		move = Vector2(spd, 0)
+	if posEnemy == 2:
+		move = Vector2(0, -spd)
+	if posEnemy == 3:
+		move = Vector2(-spd, 0)
+	if posEnemy == 4:
 		move = Vector2(0, spd)
 	pass
 
 func _physics_process(delta):
+	print(posEnemy)
+	pos_assign()
 	pathfinding()
 	move_and_slide(move)
 
@@ -26,7 +45,7 @@ func _on_Area2D_area_entered(area):
 	$Stun.start()
 	stun = true
 	if stun == true:
-		move = Vector2(0,0)
+		posEnemy = 0
 		Autoload.inmortal = true
 		Autoload.inmortal2 = true
 	pass 
@@ -36,5 +55,5 @@ func _on_Stun_timeout():
 	stun = false
 	Autoload.inmortal = false
 	Autoload.inmortal2 = false
-	move = Vector2(spd, 0)
+	posEnemy = 1
 	pass # Replace with function body.
