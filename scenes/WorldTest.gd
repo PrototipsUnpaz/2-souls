fe5568258237d1af1c46d213a9123b3057ef19f3
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-05-27 15:02:48 -0300

    Label victoria
    Se creo la escena TextWin que contiene el label con el texto correspondiente al ganar el nivel.

--- scenes/WorldTest.gd
+++ scenes/WorldTest.gd
@@ -4,11 +4,12 @@ extends Node2D
 func _ready():
 	$StickDigital.modulate = Color(1,1,1,0.4)
 	$ButtonAttack.modulate = Color(1,1,1,0.4)
+	$CenterContainer.visible = false
 	pass
 	
 func _process(delta):
 	if Autoload.count == 2:
-		get_tree().change_scene("res://scenes/game_over/GameOver.tscn")
+		$CenterContainer.visible = true
 		
 	if Autoload.dead == true:
 		get_tree().change_scene("res://scenes/game_over/GameOver.tscn")
