eb46cd9d20e2428aa8d6963fd5827a0cd111069b
negrodraco65 <52358927+negrodraco65@users.noreply.github.com>
2020-06-05 23:34:20 -0300

    Señales conectadas
    se crearon las señales correspondientes a cada boton. y se creo el script con sus funciones de salir y jugar.

---
+++ scenes/menu_main/Menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+func _on_Jugar_pressed():
+	get_tree().change_scene("res://scenes/WorldTest.tscn")
+	pass # Replace with function body.
+
+
+func _on_Creditos_pressed():
+	pass # Replace with function body.
+
+
+func _on_Salir_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
