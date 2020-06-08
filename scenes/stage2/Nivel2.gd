f2ec08d8363ae2dbd2fddb77980f56d2e77de3f5
negrodraco65 <52358927+negrodraco65@users.noreply.github.com>
2020-06-08 02:18:08 -0300

    Portal
    Se instanció portal en nivel2.

---
+++ scenes/stage2/Nivel2.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _ready():
+	$StickDigital.modulate = Color(1,1,1,0.4)
+	$ButtonAttack.modulate = Color(1,1,1,0.4)
+	$CenterContainer.visible = false
+	pass
+	
+func _process(delta):
+	if Autoload.count == 2:
+		$CenterContainer.visible = true
+		
+	if Autoload.dead == true:
+		get_tree().change_scene("res://scenes/game_over/GameOver.tscn")
+		Autoload.dead = false
+	pass 
