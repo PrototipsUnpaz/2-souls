4d3c8917b01493c4c5689f2030d0e124e9051971
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-06-12 08:02:20 -0300

    Sfx Buttons
    Se cargo un efecto de sonido en todos los botones de cada menu

--- scenes/menu_main/Menu.gd
+++ scenes/menu_main/Menu.gd
@@ -1,21 +1,29 @@
 extends Node2D
 
+var sfxButtons = preload("res://scenes/audio/sfx/SfxButtons.tscn")
+
+
 func _ready():
 	get_tree().set_auto_accept_quit(false);
 	pass
 
 func _on_Jugar_pressed():
+	var sfxBbut = sfxButtons.instance()
+	get_parent().add_child(sfxBbut)
 	get_tree().change_scene("res://scenes/WorldTest.tscn")
 	pass 
 
 
 func _on_Creditos_pressed():
-	
+	var sfxBbut = sfxButtons.instance()
+	get_parent().add_child(sfxBbut)
 	get_tree().change_scene("res://scenes/menu_main/creditos/Creditos.tscn")
 	pass 
 
 
 func _on_Salir_pressed():
+	var sfxBbut = sfxButtons.instance()
+	get_parent().add_child(sfxBbut)
 	get_tree().quit()
 	pass 
 
