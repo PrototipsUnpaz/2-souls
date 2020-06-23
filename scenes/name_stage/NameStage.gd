0a2dd222286b36521f35a36c28649269ce6eac01
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-06-23 15:17:20 -0300

    fix navegacion
    Se arreglo unas direcciones y condiciones en la escena "NameStage"

--- scenes/name_stage/NameStage.gd
+++ scenes/name_stage/NameStage.gd
@@ -6,13 +6,15 @@ func _ready():
 
 func name_stage():
 	if Autoload.lv1 == true:
-		$Control/Font.text = "Stage 1"
+		$Control/Font.text = "Nivel 1"
 	elif Autoload.lv2 == true:
-		$Control/Font.text = "Stage 2"
+		$Control/Font.text = "Nivel 2"
 	elif Autoload.lv3 == true:
-		$Control/Font.text = "Stage 3"
+		$Control/Font.text = "Nivel 3"
 	elif Autoload.lv4 == true:
-		$Control/Font.text = "Stage 4"
+		$Control/Font.text = "Nivel 4"
+	elif Autoload.winGame == true:
+		$Control/Font.text = "¡Juego completado!"
 	pass
 
 func select_stage():
@@ -23,9 +25,9 @@ func select_stage():
 	elif Autoload.lv3 == true:
 		get_tree().change_scene("res://scenes/stage3/Nivel3.tscn")
 	elif Autoload.lv4 == true:
-		get_tree().change_scene("res://scenes/stage4/Nivel4.tscn")
+		get_tree().change_scene("res://scenes/stage4/Lv4.tscn")
 	elif Autoload.winGame == true:
-		get_tree().change_scene("res://scenes/stage4/Nivel4.tscn")
+		get_tree().change_scene("res://scenes/win_game/WinGame.tscn")
 	pass
 
 func _on_NextScen_timeout():
